2b26528a500db5ae1b19ce627f8646e982a1d280
vasanthsrinu <vasanthnarsing96@gmail.com>
2020-05-22 09:42:42 +0000

    deleting unwanted files

--- proj.java
+++
@@ -1,3 +0,0 @@
-This is updated in development
-This is the new code to the project
-This is updated code
--- vasanth.sh
+++
@@ -1,4 +0,0 @@
-This is vasanth
-Am learning devops
-I am going to be a devops engineer
-Automation is very interesting concepts
